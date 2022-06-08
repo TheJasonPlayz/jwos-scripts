8efe937e0eb91beded7b1b01dd9ccab7918dd4e1
Jason Whitman <thejasonplayz@gmail.com>
2022-06-08 16:21:32 -0600

    forgot a script

---
+++ Uncategorized/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git config --global user.email "thejasonplayz@gmail.com"
+git config --global user.name "TheJasonPlayz"
+
+# CLONE DOTFILES REPO\
+mkdir ~/.git/
+git clone https://github.com/TheJasonPlayz/dotfiles.git ~/.git/dotfiles/
+
+# REMOVE LIGHTDM CACHE
+sudo rm -r /var/cache/lightdm
+
+#INSTALL DOOM EMACS
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+#INSTALL BACKGROUNDS / USER DIRS
+xdg-user-dirs-update
+git clone https://gitlab.com/dwt1/wallpapers.git ~/Pictures/Backgrounds
