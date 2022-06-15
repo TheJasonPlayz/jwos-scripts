9103c61000f6476c6ed248919c28614548d6c573
TheJasonPlayz <thejasonplayz@gmail.com>
2022-06-14 18:40:25 -0600

    updates

--- New_Installs/configure.sh
+++ New_Installs/configure.sh
@@ -3,17 +3,16 @@
 git config --global user.email "thejasonplayz@gmail.com"
 git config --global user.name "TheJasonPlayz"
 
-# CLONE DOTFILES REPO\
-mkdir ~/.git/
-git clone https://github.com/TheJasonPlayz/dotfiles.git ~/.git/dotfiles/
-
 # REMOVE LIGHTDM CACHE
 sudo rm -r /var/cache/lightdm
 
 #INSTALL DOOM EMACS
-#git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-#~/.emacs.d/bin/doom install
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+~/.emacs.d/bin/doom install
 
 #INSTALL BACKGROUNDS / USER DIRS
 xdg-user-dirs-update
 git clone https://gitlab.com/dwt1/wallpapers.git ~/Pictures/Backgrounds
+
+#UNMUTE ALSA
+amixer sset Master unmute
